106dc5c972883367e0e19b449d8aa5eb12b48f2b
dhruboxR <dhrubox656@gmail.com>
2026-03-16 21:32:19 +0600

    Updated solution

--- Introductory Problems/PalindromeReorder.cpp
+++ Introductory Problems/PalindromeReorder.cpp
@@ -2,14 +2,11 @@
 using namespace std;
 
 void solve() {
-    string str;     cin >> str; 
-    map<char, int> cfreq;
+    string str;     cin >> str;   map<char, int> cfreq;
 
     for(char cherr : str) cfreq[ cherr ]++;
     // only one character makes the whole string 
-    if(cfreq.size() == 1) {
-        cout << str << endl;    return;
-    }
+    if(cfreq.size() == 1) { cout << str << endl;    return;}
 
     int odd = 0;       char ocherr;         // at max one odd freq character is valid 
 
@@ -19,15 +16,14 @@ void solve() {
             if(odd > 1) {cout << "NO SOLUTION" << endl; return;}
         }
     }
-    // construct the palindromic string 
+
     string temp = "";
-    for(auto [ch, freq] : cfreq) {
+    for(auto [ch, freq] : cfreq) {      // construct the palindromic string 
         if(freq&1) continue;
 
-        string curr(freq/2, ch);
-        temp += curr;
+        string curr(freq/2, ch);    temp += curr;
     }
-    string rtemp = temp;    reverse( temp.begin(), temp.end());
+    string rtemp = temp;    reverse( temp.begin(), temp.end());     // front + back 
 
     if(odd == 1) {
         string oc(cfreq[ ocherr ], ocherr);
