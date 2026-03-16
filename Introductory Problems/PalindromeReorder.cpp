44d7e5e897b4a5cdd2e60096af254f56835d4611
dhruboxR <dhrubox656@gmail.com>
2026-03-16 21:27:56 +0600

    PalindromeReorder\

---
+++ Introductory Problems/PalindromeReorder.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    string str;     cin >> str; 
+    map<char, int> cfreq;
+
+    for(char cherr : str) cfreq[ cherr ]++;
+    // only one character makes the whole string 
+    if(cfreq.size() == 1) {
+        cout << str << endl;    return;
+    }
+
+    int odd = 0;       char ocherr;         // at max one odd freq character is valid 
+
+    for(auto [ch, freq] : cfreq) {
+        if(freq & 1) {
+            odd++;      ocherr = ch;
+            if(odd > 1) {cout << "NO SOLUTION" << endl; return;}
+        }
+    }
+    // construct the palindromic string 
+    string temp = "";
+    for(auto [ch, freq] : cfreq) {
+        if(freq&1) continue;
+
+        string curr(freq/2, ch);
+        temp += curr;
+    }
+    string rtemp = temp;    reverse( temp.begin(), temp.end());
+
+    if(odd == 1) {
+        string oc(cfreq[ ocherr ], ocherr);
+        cout << temp << oc << rtemp << endl;
+    }
+    else cout << temp << rtemp << endl; // all even characters 
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+    return 0;
+}
